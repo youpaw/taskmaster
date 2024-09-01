263147b5fa933153113ac7a0e4e65a4888d111b0
Vladimir Drozdov <drozdov.vov@gmail.com>
2024-09-01 23:57:04 +0200

    Monitor: enhance api
    Add custom error handling
    Add methods for start, start_by_name and update
    Fix reload_configs

--- src/monitor.py
+++ src/monitor.py
@@ -75,6 +75,10 @@ class Task:
             # self.check_stop()
             pass
 
+class MonitorError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 class Monitor:
     def __init__(self, config: Configuration):
@@ -83,46 +87,82 @@ class Monitor:
         self.old_tasks = []
         self.tasks = {}
 
+    def start_by_name(self, name: str):
+        if name not in self.tasks:
+            raise MonitorError(f"Task {name} does not exist.")
+        self._start_task(name, self.tasks[name])
+
     def start(self):
-        """Start all programs."""
-        for name, program in self.config.config.items():
-            task = Task(program)
-            task.start()
-            self.tasks[name] = task
+        """Start all tasks."""
+        active_ids = set(self.active_tasks)
+        all_ids = set(self.tasks.keys())
+        start_ids = all_ids - active_ids
+        for name in start_ids:
+            try:
+                self._start_task(name, self.tasks[name])
+            except MonitorError as _:
+                pass
+
+    def _start_task(self, name: str, task: Task):
+        if task in self.active_tasks or task.status == "STOPPING":
+            raise MonitorError(f"Task {name} is running.")
+        elif task.status == "FAILED" or task.status == "SUCCEEDED":
+            raise MonitorError(f"Task {name} is already finished.")
+        elif task.status == "RESTARTING":
+            raise MonitorError(f"Task {name} is restarting.")
+        task.start()
+        self.active_tasks.append(name)
 
     def stop(self):
         """Stop all tasks."""
         pass
 
+    def update(self):
+        for name in self.active_tasks:
+            self.tasks[name].update_status()
+
     def reload_config(self):
         """Reload the configuration."""
-        old_config = self.config
+        old_progs = self.config.programs
         self.config.reload_config()
-        new_config = self.config
-        old_ids = set(old_config.programs.keys())
-        new_ids = set(new_config.programs.keys())
+        new_progs = self.config.programs
+        # Initialize tasks
+        if not self.tasks:
+            for name, program in new_progs.items():
+                self._create_task(name, program)
+            return
+
+        old_ids = set(old_progs.keys())
+        new_ids = set(new_progs.keys())
+        # Process added programs
         added_ids = new_ids - old_ids
         for name in added_ids:
-            self.tasks[name] = task = Task(new_config.programs[name])
-            if new_config.programs[name].autostart:
-                task.start()
-                self.active_tasks.append(name)
+            self._create_task(name, new_progs[name])
+        # Process removed programs
         removed_ids = old_ids - new_ids
         for name in removed_ids:
-            task = self.tasks.pop(name)
-            if name in self.active_tasks:
-                self.active_tasks.remove(name)
-                task.stop()
-            self.old_tasks.append(task)
-        intersect_ids = new_ids & old_ids
-        for name in intersect_ids:
-            if old_config.programs[name] == new_config.programs[name]:
+            self._retire_task(name)
+        # Process same programs
+        same_ids = new_ids & old_ids
+        for name in same_ids:
+            if old_progs[name] == new_progs[name]:
                 continue
-            task = self.tasks.pop(name)
-            if name in self.active_tasks:
-                self.active_tasks.remove(name)
-                task.stop()
+            self._retire_task(name)
+            self._create_task(name, new_progs[name])
+
+    def _create_task(self, name, program: Program):
+        self.tasks[name] = task = Task(program)
+        if program.autostart:
+            task.start()
+            self.active_tasks.append(name)
+
+    def _retire_task(self, name: str):
+        task = self.tasks.pop(name)
+        if name in self.active_tasks:
+            self.active_tasks.remove(name)
+            task.stop()
             self.old_tasks.append(task)
+
 
 if __name__ == "__main__":
     conf = Configuration("/home/kosyan62/PycharmProjects/taskmaster/test/base.yaml")
@@ -131,6 +171,6 @@ if __name__ == "__main__":
     monitor.start()
     print(monitor.tasks)
     time.sleep(15)
-    for task in monitor.tasks.values():
-        task.update_status()
+    for _task in monitor.tasks.values():
+        _task.update_status()
     print(monitor.tasks)
